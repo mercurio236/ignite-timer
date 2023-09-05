6761b26a7079bc505168cdbb7aabe501324cd4d7
Arley Souto <macx15@hotmail.com>
2023-09-05 13:43:55 -0300

    Configurando as rotas usando outlet de react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import { ThemeProvider } from 'styled-components'
-import { Button } from './components/Button'
 import { defaulTheme } from './styles/themes/default'
+import { BrowserRouter } from 'react-router-dom'
+
 import { GlobalStyle } from './styles/global'
+import { Router } from './Router'
 
 export function App() {
   return (
     <ThemeProvider theme={defaulTheme}>
-      <Button />
-      <Button />
-      <Button />
-      <Button />
-      <Button />
       <GlobalStyle />
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
     </ThemeProvider>
   )
 }
