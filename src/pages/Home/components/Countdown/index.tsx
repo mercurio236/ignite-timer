c9b3c65ae0189766b98fb938905634b355b373cb
Arley Souto <macx15@hotmail.com>
2023-09-14 02:51:11 -0300

    ajustado o historico

--- src/pages/Home/components/Countdown/index.tsx
+++ src/pages/Home/components/Countdown/index.tsx
@@ -34,7 +34,7 @@ export function Countdown() {
       interval = setInterval(() => {
         const secondsDifference = differenceInSeconds(
           new Date(),
-          activeCycle.startDate
+          activeCycle.startDate,
         )
         if (secondsDifference >= totalSeconds) {
           markCurrentCycleAsFinished()
