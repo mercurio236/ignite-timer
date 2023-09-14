2711b449079dbeffaf0f6c65ba0d46ccd8cc9f00
Arley Souto <macx15@hotmail.com>
2023-09-14 02:36:19 -0300

    ajustado o context

--- src/pages/Home/components/Countdown/index.tsx
+++ src/pages/Home/components/Countdown/index.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useContext } from 'react'
 import { CountdownConainer, Separator } from './styles'
 import { differenceInSeconds } from 'date-fns'
-import { CyclesContext } from '../..'
+import { CyclesContext } from '../../../../contexts/CyclesContext'
 
 export function Countdown() {
   const {
@@ -34,7 +34,7 @@ export function Countdown() {
       interval = setInterval(() => {
         const secondsDifference = differenceInSeconds(
           new Date(),
-          activeCycle.startDate,
+          activeCycle.startDate
         )
         if (secondsDifference >= totalSeconds) {
           markCurrentCycleAsFinished()
